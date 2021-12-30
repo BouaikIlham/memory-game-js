a82bb55e9b3152fa104996fc835ca3cbbbe06d84
ilham <ibouaik@gmail.com>
2021-12-30 20:22:23 +0100

    add message

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     
-    const cardArray = [
+    let cardArray = [
       {
         name: 'fries',
         img: 'images/fries.png'
@@ -50,9 +50,18 @@ document.addEventListener('DOMContentLoaded', () => {
         img: 'images/hotdog.png'
       }
     ]
-    // cardArray.sort(() => 0.5 - Math.random)
+
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    shuffleArray(cardArray)
     const grid = document.getElementById("grid")
-    const resultDisplay = document.getElementById("result")
+    const resultDisplay = document.getElementById("score")
+    const message = document.getElementById("message")
     let cardsChosen = []
     let cardsChosenId = []
     let cardsWon = []
@@ -75,21 +84,22 @@ document.addEventListener('DOMContentLoaded', () => {
      const optionOneId = cardsChosenId[0]
      const optionTowId = cardsChosenId[1]
      if (cardArray[cardsChosenId[0]].img === cardArray[cardsChosenId[1]].img) {
-         alert ('you found a match')
+      
+        message.textContent = "you found a match"
          cards[optionOneId].setAttribute('src', 'images/white.png')
          cards[optionTowId].setAttribute('src', 'images/white.png')
          cardsWon.push(cardsChosen)
      } else {
-         cards[optionOneId].setAttribute('src', 'images/mask.png')
-         cards[optionTowId].setAttribute('src', 'images/mask.png')
-         alert('sorry, try again')
+        cards[optionOneId].setAttribute('src', 'images/mask.png')
+        cards[optionTowId].setAttribute('src', 'images/mask.png')
+        message.textContent = "sorry, try again"
      }
      cardsChosen = []
      cardsChosenId = []
      resultDisplay.textContent = cardsWon.length
-    //  if (cardsWon.length === cardsWon.length/2) {
-    //     resultDisplay.textContent = 'congratulation!! you found them all!!'
-    //  }
+     if (cardsWon.length === cardArray.length/2) {
+        resultDisplay.textContent = 'congratulation!! you found them all!!'
+     }
      }
 
 // flip your card
@@ -101,7 +111,6 @@ document.addEventListener('DOMContentLoaded', () => {
     if (cardsChosen.length === 2) {
         setTimeout(CheckForMatch, 500)
     }
-
 
     }
  
